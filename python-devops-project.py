7b38f5eca5690be09d11a9920f5cc46c222064e0
ziad-elmelegy <ziadelmelegy42@gmail.com>
2026-04-20 12:46:57 +0200

    eat: add dictionary and loop examples with user input

--- python-devops-project.py
+++ python-devops-project.py
@@ -6,13 +6,17 @@ Server = input("choose your Server: ")
 ip = input("Type your IP address: ")
 port = int(input("Choose Your Port: "))
 memory = float(input("choose your memory: "))
-is_running =  True
-cpu_threshold = 80.5
+
+dict = {
+"Server": Server,
+"IP": ip,
+"port": port,
+"is_running": True,
+"memory": f"{memory} GB",
+"cpu_threshold": 80.5
+}
 
 print("=" * 80)
 
-print(f"Server: {Server}")
-print(f"IP: {ip}")
-print(f"Port: {port}")
-print(f"Running: {is_running}")
-print(f"CPU Threshold: {cpu_threshold}%\n")
+for key, value in dict.items():
+    print(f"{key:15}: {value}")
